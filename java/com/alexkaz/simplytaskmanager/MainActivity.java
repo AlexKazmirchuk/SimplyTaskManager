853f54ed3af60c621000e0b4a7d9756bbdc20c2b
AlexKazmirchuk <zedomo@mail.ru>
2016-08-19 09:27:01 +0800

    Initial commit

---
+++ java/com/alexkaz/simplytaskmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.alexkaz.simplytaskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
