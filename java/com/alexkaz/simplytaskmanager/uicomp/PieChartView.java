95fb89f1c3a372928eedfe7d485100fdbf767573
AlexKazmirchuk <zedomo@mail.ru>
2016-08-24 17:05:07 +0800

    now a pie chart drawn like as on a template

--- java/com/alexkaz/simplytaskmanager/uicomp/PieChartView.java
+++ java/com/alexkaz/simplytaskmanager/uicomp/PieChartView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,10 +14,12 @@ public class PieChartView extends View {
 
     public PieChartView(Context context) {
         super(context);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     public PieChartView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     @Override
@@ -26,17 +30,33 @@ public class PieChartView extends View {
         p.setColor(Color.RED);
         p.setAntiAlias(true);
 
-//        canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,38,p);
 
         p.setColor(Color.GREEN);
 
-        RectF rectF = new RectF(canvas.getWidth()/2-38,canvas.getHeight()/2-38,canvas.getWidth()/2+38,canvas.getHeight()/2+38);
+        RectF rectF = new RectF(canvas.getWidth()/2-40,canvas.getHeight()/2-40,canvas.getWidth()/2+40,canvas.getHeight()/2+40);
 
         p.setColor(Color.GREEN);
-        canvas.drawArc(rectF,180,-170,true,p);
+        canvas.drawArc(rectF,270,-120,true,p);
 
         p.setColor(Color.YELLOW);
-        canvas.drawArc(rectF,10,-70,true,p);
+        canvas.drawArc(rectF,150,-120,true,p);
+
+        p.setColor(Color.RED);
+        canvas.drawArc(rectF,30,-120,true,p);
+
+
+        Paint p1 = new Paint();
+        p1.setStyle(Paint.Style.STROKE);
+        p1.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+        p1.setAntiAlias(true);
+        p1.setStrokeWidth(4);
+        p1.setColor(Color.BLACK);
+
+        canvas.drawArc(rectF,270,-120,true,p1);
+        canvas.drawArc(rectF,150,-120,true,p1);
+        canvas.drawArc(rectF,30,-120,true,p1);
+
+
 
 
     }
