927b7c1b4d82fdd5e4affe78636d8b3ded4a9e6d
AlexKazmirchuk <zedomo@mail.ru>
2016-08-25 14:50:49 +0800

    both actionBars fixed

--- java/com/alexkaz/simplytaskmanager/FullTaskActivity.java
+++ java/com/alexkaz/simplytaskmanager/FullTaskActivity.java
@@ -2,6 +2,7 @@ package com.alexkaz.simplytaskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class FullTaskActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class FullTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_task);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- java/com/alexkaz/simplytaskmanager/MainActivity.java
+++ java/com/alexkaz/simplytaskmanager/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alexkaz.simplytaskmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_developer_board_white_24dp);
+    }
+
+    public void startSecondActivity(View view) {
+        Intent intent = new Intent(this,FullTaskActivity.class);
+        startActivity(intent);
     }
 }
